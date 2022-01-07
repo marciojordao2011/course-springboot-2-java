15b61b5da4f7bfb236a4455368690192da37941a
unknown <marcio.jordao@hotmail.com>
2022-01-07 14:50:26 -0300

    Category

--- src/main/java/com/jordaoweb/course/config/TestConfig.java
+++ src/main/java/com/jordaoweb/course/config/TestConfig.java
@@ -1,5 +1,7 @@
 package com.jordaoweb.course.config;
 import java.time.Instant;
+import com.jordaoweb.course.repositories.CategoryRepository;
+
 import java.util.Arrays;
 import  com.jordaoweb.course.entities.enums.OrderStatus ;
 import com.jordaoweb.course.repositories.OrderRepository;
@@ -10,7 +12,7 @@ import org.springframework.context.annotation.Profile;
 import com.jordaoweb.course.entities.Order;
 import com.jordaoweb.course.entities.User;
 import com.jordaoweb.course.repositories.UserRepository;
-
+import com.jordaoweb.course.entities.Category;
 @Configuration
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
@@ -20,8 +22,18 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
