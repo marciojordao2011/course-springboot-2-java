be26d2c26e8882f4b2894e24b56ed83955c6f2b2
unknown <marcio.jordao@hotmail.com>
2022-01-09 18:25:27 -0300

    Exception handling - update

--- src/main/java/com/jordaoweb/course/services/UserService.java
+++ src/main/java/com/jordaoweb/course/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import com.jordaoweb.course.services.exceptions.ResourceNotFoundException;
 import com.jordaoweb.course.services.exceptions.DatabaseException;
+import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -42,9 +43,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getOne(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}	
 	}
 
 	private void updateData(User entity, User obj) {
